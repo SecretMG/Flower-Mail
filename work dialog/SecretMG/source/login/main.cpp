741a44b0142728ed632ac9be887a4fb3c87f0597
SecretMG <reburnming@126.com>
2020-09-09 23:30:24 +0800

    09-09

---
+++ work dialog/SecretMG/source/login/main.cpp
@@ -0,0 +1,33 @@
+#include "mainwindow.h"
+#include "logindialog.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    LoginDialog dlg;
+    if (dlg.exec() == QDialog::Accepted){
+        w.show();
+        return a.exec();
+    }
+    else
+        return 0;
+}
+
+
+/*
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    LoginDialog dlg;
+    if (dlg.exec() == QDialog::Accepted)
+    {
+       w.show();
+       return a.exec();
+    }
+    else return 0;
+}
+*/
